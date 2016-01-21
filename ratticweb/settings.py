4ba58a69c8f242e279cf3fbfaf990c232dec6c22
kyle lau <kylexlau@gmail.com>
2016-01-21 17:21:06 +0800

    change date time format
    better data time format for us

--- ratticweb/settings.py
+++ ratticweb/settings.py
@@ -50,7 +50,11 @@ MANAGERS = ADMINS
 
 # The Internationalization Settings
 USE_I18N = True
-USE_L10N = True
+#USE_L10N = True
+# better date format
+USE_L10N = False
+DATETIME_FORMAT = 'Y-m-d H:i:s'
+
 LOCALE_PATHS = (
     'conf/locale',
 )
